d11bbdd0f3cb4d6604bf8454f0f408d4bcbe1ecd
maruyu5 <t.kawakami@yubun.co.jp>
2024-03-13 16:50:38 +0900

    __init__.pyが効いていないことが判明したので、別の方法で開発環境と本番環境を判断する修正を行った

--- venv_design_search/design_search/design_search_project/__init__.py
+++
@@ -1,6 +0,0 @@
-from .settings import *                  # settings.py(本番環境用)をimport
-
-try:
-    from .settings_dev import *      # settings_dev.pyが存在しないか、その他のエラーが発生した場合は、settings_dev.py(開発環境用)をimport
-except:
-    pass
--- venv_design_search/design_search/design_search_project/settings.py
+++ venv_design_search/design_search/design_search_project/settings.py
@@ -1,4 +1,4 @@
 from .settings_common import *
 
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['49.212.144.204']
